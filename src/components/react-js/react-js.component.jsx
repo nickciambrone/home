200939c1363d5b548dc3ab9d466c5d8b4b7e93c4
Nick Ciambrone <nickciambrone@Nicks-MBP.fios-router.home>
2020-08-10 12:07:47 -0400

    front end finishing tou

--- src/components/react-js/react-js.component.jsx
+++ src/components/react-js/react-js.component.jsx
@@ -8,7 +8,7 @@ const ReactJS = () => {
                     <div className='col-6' style={{padding:0}}>
                     <div className = 'appLink'><a href='https://royal-cloth.herokuapp.com/shop' className='' target='_blank'>eCommerce Web App</a></div>
 
-                        <img src={require('./royalCloth.png')} width='100%' />
+                    <img src={require('./royalCloth.png')} width='100%' />
 
 
                     </div>
@@ -20,6 +20,16 @@ const ReactJS = () => {
 
                     </div>
 
+                </div>
+                <div className = 'row' style={{padding:'12px'}}>
+                    <div className='col-6'>
+                        <div className = 'appLink'><a href='https://github.com/nickciambrone/pigeon' className='' target='_blank'>Pigeon</a></div>
+                        <img src='https://cdn.pixabay.com/photo/2013/07/12/19/33/carrier-pigeon-154970_960_720.png' width='100%' height='450px'/>
+
+                    </div>
+                    <div className ='col-6'>
+
+                    </div>
                 </div>
 
 
